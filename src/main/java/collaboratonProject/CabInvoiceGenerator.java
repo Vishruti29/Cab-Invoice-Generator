3b9ff9a89df0583f7222ba9ab474d4cc166f341f
Vishruti Patil <vishruti2908@gmail.com>
2023-04-13 21:42:15 +0530

    Cab Invoice Generator Program

--- src/main/java/collaboratonProject/CabInvoiceGenerator.java
+++ src/main/java/collaboratonProject/CabInvoiceGenerator.java
@@ -1,7 +1,6 @@
 package collaboratonProject;
 
 public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Cab Invoice Generator Collaboration Project");
-    }
+
+
 }
