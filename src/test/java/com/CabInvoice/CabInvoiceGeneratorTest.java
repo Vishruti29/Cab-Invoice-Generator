67e2b2df44f7f48c81b8bf4e7aa3e96479ccdc2c
ASHU-code06 <ashuc306@gmail.com>
2023-04-16 14:03:51 +0530

    Added test case for checking the valid fare(UC1) return when given a particular time and distance

--- src/test/java/com/CabInvoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/CabInvoice/CabInvoiceGeneratorTest.java
@@ -1,1 +1,16 @@
 package com.CabInvoice;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CabInvoiceGeneratorTest{
+    @Test
+    public void givenDistanceAndTime_ShouldReturnValidFare(){
+        CabInvoiceGenerator cig = new CabInvoiceGenerator();
+        double result = cig.calculateFare(50,20);
+        Assertions.assertEquals(520,result);
+        double check = cig.calculateFare(0,2);
+        Assertions.assertEquals(5,check);
+    }
+}
