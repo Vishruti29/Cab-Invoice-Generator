08d71d9810494875a537a043d9d6068722bf523b
Vishruti Patil <vishruti2908@gmail.com>
2023-04-16 18:17:50 +0530

    Test case added for modified case of invoice generation for multiple Rides

--- src/main/java/com/CabInvoice/CabInvoiceGenerator.java
+++ src/main/java/com/CabInvoice/CabInvoiceGenerator.java
@@ -1,4 +1,13 @@
 package com.CabInvoice;
+class Ride {
+    double distance;
+    int time;
+
+    Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
 public class CabInvoiceGenerator {
     public static final double COSTPerKM = 10.0;
     public static final double COSTPerMIN = 1.0;
@@ -9,6 +18,14 @@ public class CabInvoiceGenerator {
         double totalFare = (distance * COSTPerKM) + (time * COSTPerMIN);
         if (totalFare < 5) {
             return MIN_FARE;
+        }
+        return totalFare;
+    }
+    // UC-2 method to check the total fare for multiple rides
+    public double calculateAggregateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.distance, ride.time);
         }
         return totalFare;
     }
--- src/test/java/com/CabInvoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/CabInvoice/CabInvoiceGeneratorTest.java
@@ -1,1 +1,29 @@
 package com.CabInvoice;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class CabInvoiceGeneratorTest {
+    private CabInvoiceGenerator cabInvoiceGenerator;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnFare() {
+        double distance = 2.0;
+        int time = 5;
+        double result = cabInvoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, result, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturnAggregateFare() {
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(3.0, 8),
+                new Ride(5.0, 10) };
+        double result = cabInvoiceGenerator.calculateAggregateFare(rides);
+        Assertions.assertEquals(123.0, result, 0);
+    }
+}
